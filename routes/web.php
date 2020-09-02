bb791fe137f34e9da9d655edd573e16541e6665c
sherifNabil <sherifNabil@gitlab.com>
2020-09-02 15:47:56 +0200

    make auth and checkout feature test

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,13 @@ Route::post('books', 'BooksController@store');
 Route::patch('books/{book}', 'BooksController@update');
 Route::delete('books/{book}', 'BooksController@destroy');
 
+Route::post('/checkout/{book}', 'CheckoutBookController@store');
+Route::post('/checkin/{book}', 'CheckinBookController@store');
+
+
 // author routes
 Route::post('author', 'AuthorsController@store');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
